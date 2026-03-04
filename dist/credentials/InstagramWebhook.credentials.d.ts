7efaf054ce47304beff911dea36acbf6945eb7a4
MookieLian <mookielianhd@gmail.com>
2026-03-04 23:57:50 +0300

    chore: update lint command and bump dependencies for typescript, eslint, and n8n-node-cli

--- dist/credentials/InstagramWebhook.credentials.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ICredentialType, INodeProperties, Icon } from 'n8n-workflow';
-export declare class InstagramWebhook implements ICredentialType {
-    name: string;
-    displayName: string;
-    icon: Icon;
-    documentationUrl: string;
-    properties: INodeProperties[];
-}
